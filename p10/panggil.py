f228fc85cf566da1416de1955c13d7c5db835efc
allieoetsman72-droid <allieoetsman72@gmail.com>
2025-11-26 11:16:39 +0700

    Add files via upload

---
+++ p10/panggil.py
@@ -0,0 +1,16 @@
+import bangunruang as br
+import bangundatar as bd
+
+print("~~~bangunruang~~~")
+print(f"Volum kubus dengan sisi 3 adalah {br.kubus}(3)")
+print(f"Volum balok adalah {br.balok}(4)")
+print(f"Volum prisma segitiga adalah {br.prisma}(4,5,6)")
+print(f"Volum tabung adalah ")
+print(f"Volum kerucut adalah")
+
+print("~~~bangundatar~~~")
+print(f"luas persegi adalah")
+print(f"luas persegi panjang adalah")
+print(f"luas segitiga adalah")
+print(f"luas lingkaran adalah")
+print(f"luas jajarganjar adalah {bd.jajarganjar(4,7)}")
